cab9f00b46e1dd0e836b6a8439d9bf7e48b424c1
ArisaDes <56357961+ArisaDes@users.noreply.github.com>
2019-12-05 14:03:28 +0300

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+package arisahelloworld;
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Я вас категорически приветствую, я Алиса Баштанникова");
+    }
+}
